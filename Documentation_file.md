f02e1a396a9ee2cf7985af70f2aab5600af61f53
Ayo <ayobamidele006@gmail.com>
2022-07-19 18:24:00 +0100

    add cleaning documentation,update readme, more assessing function in the script, and report file

---
+++ Documentation_file.md
@@ -0,0 +1,4 @@
+## Assessment for cleaning
+1. Convert started and ended at to datetime data type 
+2. drop the geo corrdinates columns 
+3. rename the colnmames to be more descriptive 
--- R_script.R
+++ R_script.R
@@ -1,7 +1,20 @@
-library(tidyselect)
+# Importing the packages 
+library(tidyverse)
 library(janitor)
 library(skimr)
-df <- read.csv('E:/Alx-project3/202205-divvy-tripdata/202205-divvy-tripdata.csv')
+library(lubridate)
 install.packages('usethis')
-library(usethis)
+library(usethis) # importing usethis packages to store my git credentials 
 use_usethis()
+# Reading the dataset
+df <- read.csv('E:/Alx-project3/202205-divvy-tripdata/202205-divvy-tripdata.csv')
+
+# Lets view the dataset head
+head(df, 5)
+View(df)
+
+# exploring the dataset
+str(df)
+colnames(df) # getting the column names in the dataframe
+glimpse(df)
+skim_without_charts(df)
